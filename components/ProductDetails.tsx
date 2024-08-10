ca61762a2d62ed5c5c9728b23dd2fadf3d5b9824
Ravi Anuragi <anuragiravi85@gmail.com>
2024-08-10 15:17:38 +0530

    key issue in product details

--- components/ProductDetails.tsx
+++ components/ProductDetails.tsx
@@ -14,35 +14,39 @@ const ProductDetails = ({ product }: IProps) => {
             <div className="md:w-1/2 h-fit">
               <p className="text-xl font-semibold mb-5">Technical Details</p>
               <table>
-                {product?.productInformationTech?.map((item, index) => (
-                  <tr key={index}>
-                    <th className="bg-gray-100 border p-2 font-medium text-left pl-5">
-                      {item.name}
-                    </th>
-                    <td className="border p-2 font-normal text-left pl-5">
-                      {item.value}
-                    </td>
-                  </tr>
-                ))}
+                <tbody>
+                  {product?.productInformationTech?.map((item, index) => (
+                    <tr key={index}>
+                      <th className="bg-gray-100 border p-2 font-medium text-left pl-5">
+                        {item.name}
+                      </th>
+                      <td className="border p-2 font-normal text-left pl-5">
+                        {item.value}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
               </table>
             </div>
             {product?.productInformationAdditional?.length > 1 && (
               <div className="md:w-1/2 h-fit">
                 <p className="text-xl font-semibold mb-5">Additional Details</p>
                 <table>
-                  {product?.productInformationAdditional.map(
-                    (item, index) =>
-                      item.name !== "Customer Reviews" && (
-                        <tr key={index}>
-                          <th className="bg-gray-100 border p-2 font-medium text-left pl-5">
-                            {item.name}
-                          </th>
-                          <td className="border p-2 font-normal text-left pl-5">
-                            {item.value}
-                          </td>
-                        </tr>
-                      )
-                  )}
+                  <tbody>
+                    {product?.productInformationAdditional.map(
+                      (item, index) =>
+                        item.name !== "Customer Reviews" && (
+                          <tr key={index}>
+                            <th className="bg-gray-100 border p-2 font-medium text-left pl-5">
+                              {item.name}
+                            </th>
+                            <td className="border p-2 font-normal text-left pl-5">
+                              {item.value}
+                            </td>
+                          </tr>
+                        )
+                    )}
+                  </tbody>
                 </table>
               </div>
             )}
