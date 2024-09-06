80e1c94b4de2754ca837395208f185f12010a5fe
Ravi Anuragi <anuragiravi85@gmail.com>
2024-09-06 09:47:21 +0530

    change in home

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,19 +1,11 @@
-import { useGetEmail } from "@/features/email/use-get-email";
 import { useTranslations } from "next-intl";
 import React from "react";
-import { Button } from "./ui/button";
 
 const Home = () => {
   const t = useTranslations("PhotoViewer");
-  const { refetch, isLoading, isError, data, error } = useGetEmail();
-
-  const handleSendEmail = () => {
-    refetch();
-  };
 
   return (
     <div>
-      <Button onClick={handleSendEmail}>Send email</Button>
       <h1>{t("title")}</h1>
       <p>{t("description")}</p>
     </div>
